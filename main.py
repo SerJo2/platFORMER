5c5ddb0df62a660a0d0ede42f74fd250de1f61a3
SerJo <termenator1ser@gmail.com>
2021-02-14 21:22:49 +1000

    bad mov

--- main.py
+++ main.py
@@ -14,15 +14,37 @@ pygame.display.set_caption('platFORMER')
 
 player_image = pygame.image.load('player.png')
 
+mov_right = False
+mov_left = False
+cords = [50, 50]
+
 while True:
     
     for event in pygame.event.get():
+        screen.fill((146,244,255))
 
-        screen.blit(player_image, (50, 50))
+        if mov_left:
+            cords[0] -= 4
+        if mov_right:
+            cords[0] += 4
+        screen.blit(player_image, cords)
 
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                mov_right = True
+            if event.key == K_LEFT:
+                mov_left = True
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                mov_right = False
+            if event.key == K_LEFT:
+                mov_left = False
+
+
+
 
     pygame.display.update()
     clock.tick(60)
