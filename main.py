0c41685d5e3618dbe930bccad5774d7bb33597c1
SerJo <termenator1ser@gmail.com>
2021-02-14 21:13:19 +1000

    new pic player

--- main.py
+++ main.py
@@ -12,9 +12,13 @@ WINDOW_SIZE = (400, 400)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 pygame.display.set_caption('platFORMER')
 
+player_image = pygame.image.load('player.png')
+
 while True:
     
     for event in pygame.event.get():
+
+        screen.blit(player_image, (50, 50))
 
         if event.type == QUIT:
             pygame.quit()
