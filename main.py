d667113e0ba292ddbeaf4e398c43fdcf888bf240
SerJo <termenator1ser@gmail.com>
2021-02-15 22:48:06 +1000

    collide test + basic phys

--- main.py
+++ main.py
@@ -17,15 +17,31 @@ mov_left = False
 cords = [50, 50]
 player_v_momentum = 0
 
+player_rect = pygame.Rect(cords[0], cords[1], player_image.get_width(), player_image.get_height())
+test_rect = pygame.Rect(100, 100, 50, 100)
 while True:
     screen.fill((146, 244, 255))
-
     screen.blit(player_image, cords)
 
     if mov_left:
         cords[0] -= 4
     if mov_right:
         cords[0] += 4
+
+    if cords[1] > WINDOW_SIZE[1] - player_image.get_height():
+        player_v_momentum = -player_v_momentum
+        pass
+    else:
+        player_v_momentum += 0.2
+    cords[1] += player_v_momentum
+
+    player_rect.x = cords[0]
+    player_rect.y = cords[1]
+
+    if player_rect.colliderect(test_rect):
+        pygame.draw.rect(screen, (255, 0, 0), test_rect)
+    else:
+        pygame.draw.rect(screen, (0, 0, 0), test_rect)
 
     for event in pygame.event.get():
         if event.type == QUIT:
